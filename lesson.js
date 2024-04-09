6d498a31717ed09562e45dbcc1aab745a7980939
OJKoteng <ole_jacob_96@hotmail.com>
2024-04-09 11:35:38 +0200

    La til IF/Else statements i Javascript

---
+++ lesson.js
@@ -0,0 +1,25 @@
+// if/else statement 
+
+let number = 10; 
+
+// = erklærer en verdi
+// == sjekker innhold 
+// === sjekker innhold og datatype 
+// < mindre enn / less than 
+// > større enn / more than 
+
+
+if (number === 10) {
+console.log("Dette tallet er akkurat 10");
+}
+
+else if (number < 10) {
+console.log ("Dette tallet er mindre enn 10")
+}
+
+else if (number > 10) { console.log("Dette tallet er større enn 10")
+}
+
+else {
+console.log ("Dette tallet er IKKE 10")
+}
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+const submitValue = document.querySelector("#submitValue");
+console.log(submitValue); 
 
+submitValue.addEventListener
